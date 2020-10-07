d342549648eb6a8fd7186780a495b0e51c8046b7
Borna Cafuk <borna.cafuk@gmail.com>
2020-10-07 11:09:09 +0200

    Add Processor class

---
+++ hw01/src/main/java/hr/fer/oprpp1/custom/collections/Processor.java
@@ -0,0 +1,15 @@
+package hr.fer.oprpp1.custom.collections;
+
+/**
+ * A generic class to be extended and used in the strategy pattern.
+ *
+ * @author Borna Cafuk
+ */
+public class Processor {
+    /**
+     * Processes an object, the specific implementation varies by class.
+     *
+     * @param value an object to process
+     */
+    public void process(Object value) {}
+}
