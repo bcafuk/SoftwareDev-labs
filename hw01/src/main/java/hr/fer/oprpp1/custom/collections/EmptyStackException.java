888c39370477d6b422ca079e27a58270067fd354
Borna Cafuk <borna.cafuk@gmail.com>
2020-10-14 14:54:06 +0200

    Remove unused constructors

--- hw01/src/main/java/hr/fer/oprpp1/custom/collections/EmptyStackException.java
+++ hw01/src/main/java/hr/fer/oprpp1/custom/collections/EmptyStackException.java
@@ -2,7 +2,4 @@ package hr.fer.oprpp1.custom.collections;
 
 public class EmptyStackException extends RuntimeException {
     public EmptyStackException() {}
-    public EmptyStackException(String message) {super(message);}
-    public EmptyStackException(String message, Throwable cause) {super(message, cause);}
-    public EmptyStackException(Throwable cause) {super(cause);}
 }
