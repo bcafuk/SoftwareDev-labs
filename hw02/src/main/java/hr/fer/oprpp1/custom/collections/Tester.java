bbedfe5bb9e4f7f4f9dfdf036860a9b9f0ed888f
Borna Cafuk <borna.cafuk@gmail.com>
2020-10-23 20:15:53 +0200

    Add Tester class

---
+++ hw02/src/main/java/hr/fer/oprpp1/custom/collections/Tester.java
@@ -0,0 +1,10 @@
+package hr.fer.oprpp1.custom.collections;
+
+/**
+ * A generic class to be extended and used in the strategy pattern.
+ *
+ * @author Borna Cafuk
+ */
+public interface Tester {
+    boolean test(Object obj);
+}
