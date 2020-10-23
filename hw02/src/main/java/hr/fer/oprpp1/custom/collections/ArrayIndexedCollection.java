93eaa66b54b4d630f49e7d21040d74469327fd2e
Borna Cafuk <borna.cafuk@gmail.com>
2020-10-23 15:28:45 +0200

    Copy required classes from first homework

---
+++ hw02/src/main/java/hr/fer/oprpp1/custom/collections/ArrayIndexedCollection.java
@@ -0,0 +1,230 @@
+package hr.fer.oprpp1.custom.collections;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * An array-backed collection with constant-time lookup.
+ * <p>
+ * This collection may contain duplicate elements, but not {@code null} references.
+ *
+ * @author Borna Cafuk
+ */
+public class ArrayIndexedCollection extends Collection {
+    /**
+     * The default capacity (i.e. the size of the internal array) when no capacity is specified in the constructor.
+     */
+    private static final int DEFAULT_CAPACITY = 16;
+    /**
+     * By how much the capacity is multiplied when an element is to be added to an already full array.
+     */
+    private static final int GROWTH_FACTOR = 2;
+
+    /**
+     * How many elements are contained in the collection, i.e. how many indices of the internal array are occupied.
+     */
+    private int size;
+    /**
+     * The internal array containing the elements of the collection. Unused indices are set to {@code null}.
+     */
+    private Object[] elements;
+
+    /**
+     * Constructs an array collection with the default initial capacity of
+     * {@value ArrayIndexedCollection#DEFAULT_CAPACITY}.
+     */
+    public ArrayIndexedCollection() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    /**
+     * Constructs an array collection with the specified initial capacity.
+     *
+     * @param initialCapacity the initial capacity; must be 1 or greater
+     * @throws IllegalArgumentException if the specified initial capacity is less than 1
+     */
+    public ArrayIndexedCollection(int initialCapacity) {
+        if (initialCapacity < 1)
+            throw new IllegalArgumentException("The initial size of the collection must be at least 1, but " + initialCapacity + "was given.");
+
+        size = 0;
+        elements = new Object[initialCapacity];
+    }
+
+    /**
+     * Constructs an array collection from the elements of another collection.
+     *
+     * @param other the collection whose elements to insert into the new array collection
+     * @throws NullPointerException if {@code other} is {@code null}
+     */
+    public ArrayIndexedCollection(Collection other) {
+        this(other, 1);
+    }
+
+    /**
+     * Constructs an array collection from the elements of another collection.
+     * <p>
+     * This constructor takes a minimum initial capacity. If the specified collection has more elements,
+     * then the number of its elements is taken as the initial capacity instead.
+     *
+     * @param other           the collection whose elements to insert into the new array collection
+     * @param initialCapacity the minimum initial capacity
+     * @throws NullPointerException if {@code other} is {@code null}
+     */
+    public ArrayIndexedCollection(Collection other, int initialCapacity) {
+        this(Math.max(other.size(), initialCapacity));
+
+        this.addAll(other);
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean contains(Object value) {
+        return indexOf(value) != -1;
+    }
+
+    /**
+     * Adds an element to the end of the array.
+     *
+     * @param value the element to add
+     * @throws NullPointerException if the element is {@code null}
+     */
+    @Override
+    public void add(Object value) {
+        insert(value, size);
+    }
+
+    /**
+     * Gets the element at the specified index.
+     *
+     * @param index the index of the element to get
+     * @return the element at the index
+     * @throws IndexOutOfBoundsException if the index is less than 0 or if it is beyond the end of the array
+     */
+    public Object get(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException("Valid indices are 0 to " + (size - 1) + ", but " + index + " was passed.");
+
+        return elements[index];
+    }
+
+    /**
+     * Inserts an element into the array at a specified position.
+     * <p>
+     * All elements that are currently at or after the position get shifted towards the end of the array.
+     * Afterwards, the inserted element will have the specified index.
+     *
+     * @param value    the element to insert
+     * @param position the position where to insert the element
+     * @throws NullPointerException if the element is {@code null}
+     */
+    public void insert(Object value, int position) {
+        Objects.requireNonNull(value, "null cannot be inserted into collection.");
+
+        if (position < 0 || position > size)
+            throw new IndexOutOfBoundsException("Valid positions are 0 to " + size + ", but " + position + " was passed.");
+
+        growIfNecessary();
+
+        if (position != size)
+            System.arraycopy(elements, position, elements, position + 1, size - position);
+
+        elements[position] = value;
+        size++;
+    }
+
+    /**
+     * Finds the first occurrence of an element in the array and returns its index.
+     * <p>
+     * Whether an object in the collection is equal to the parameter is determined using the
+     * {@link Object#equals(Object)} method. The parameter may be {@code null}, in which case -1 is returned.
+     *
+     * @param value the element to find
+     * @return the index of the element if it exists in the collection, -1 otherwise
+     */
+    public int indexOf(Object value) {
+        if (value == null)
+            return -1;
+
+        for (int i = 0; i < size; i++)
+            if (value.equals(elements[i]))
+                return i;
+
+        return -1;
+    }
+
+    /**
+     * Removes the first occurrence of an object from the collection.
+     * Whether an object in the collection is equal to the parameter is determined using the
+     * {@link Object#equals(Object)} method.
+     *
+     * @param value the object to be removed
+     * @return {@code true} if an occurrence of {@code value} was found and removed, {@code false} otherwise
+     */
+    @Override
+    public boolean remove(Object value) {
+        int index = indexOf(value);
+
+        if (index == -1)
+            return false;
+
+        remove(index);
+        return true;
+    }
+
+    /**
+     * Removes the element at the specified index.
+     * All elements that are currently after the index get shifted towards the start of the array.
+     *
+     * @param index the index at which to remove the element
+     * @throws IndexOutOfBoundsException if the index is less than 0 or if it is beyond the end of the array
+     */
+    public void remove(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException("Valid indices are 0 to " + (size - 1) + ", but " + index + " was passed.");
+
+        if (index != size - 1)
+            System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        size--;
+        elements[size] = null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(elements, size);
+    }
+
+    /**
+     * Runs a {@link Processor}'s {@link Processor#process(Object)} method for every object in the collection,
+     * in order of ascending index.
+     *
+     * @param processor the {@link Processor} to use
+     */
+    @Override
+    public void forEach(Processor processor) {
+        for (int i = 0; i < size; i++)
+            processor.process(elements[i]);
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+        Arrays.fill(elements, null);
+    }
+
+    /**
+     * Increases the size of the array {@value ArrayIndexedCollection#GROWTH_FACTOR} times if it is full.
+     */
+    private void growIfNecessary() {
+        if (size < elements.length)
+            return;
+
+        Object[] newElements = new Object[elements.length * GROWTH_FACTOR];
+        System.arraycopy(elements, 0, newElements, 0, size);
+        elements = newElements;
+    }
+}
---
+++ hw02/src/main/java/hr/fer/oprpp1/custom/collections/Collection.java
@@ -0,0 +1,103 @@
+package hr.fer.oprpp1.custom.collections;
+
+/**
+ * A generic collection of objects.
+ *
+ * @author Borna Cafuk
+ */
+public class Collection {
+    /**
+     * This constructor ensures that {@link Collection}s cannot be instantiated except by subclasses.
+     * <p>
+     * At this point in the lesson plan, abstract classes haven't yet been covered, so I used protected constructors and
+     * dummy implementations of methods that are expected to be overridden, as instructed in the assignment document.
+     */
+    protected Collection() {}
+
+    /**
+     * Returns whether the collection is empty.
+     *
+     * @return {@code true} if the collection is empty, {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * Returns the size of the collection.
+     *
+     * @return the number of objects currently stored in the collection
+     */
+    public int size() {
+        return 0;
+    }
+
+    /**
+     * Adds an object into the collection. A collection may contain multiple occurrences of an object.
+     *
+     * @param value the object to be added
+     */
+    public void add(Object value) {}
+
+    /**
+     * Tests whether the collection contains an object.
+     * <p>
+     * Whether an object in the collection is equal to the parameter is determined using the
+     * {@link Object#equals(Object)} method.
+     *
+     * @param value the object to be tested, may be {@code null}
+     * @return {@code true} if the collection contains {@code value}, {@code false} otherwise
+     */
+    public boolean contains(Object value) {
+        return false;
+    }
+
+    /**
+     * Removes one occurrence of an object from the collection.
+     * <p>
+     * Whether an object in the collection is equal to the parameter is determined using the
+     * {@link Object#equals(Object)} method.
+     *
+     * @param value the object to be removed
+     * @return {@code true} if an occurrence of {@code value} was found and removed, {@code false} otherwise
+     */
+    public boolean remove(Object value) {
+        return false;
+    }
+
+    /**
+     * Creates an array from the collection.
+     *
+     * @return an array containing all objects contained in the collection; never {@code null}
+     */
+    public Object[] toArray() {
+        throw new UnsupportedOperationException("toArray is not supported for the base Collection class.");
+    }
+
+    /**
+     * Runs a {@link Processor}'s {@link Processor#process(Object)} method for every object in the collection.
+     *
+     * @param processor the {@link Processor} to use
+     */
+    public void forEach(Processor processor) {}
+
+    /**
+     * Adds all elements from another collection into itself.
+     *
+     * @param other the collection to add the elements from; remains unchanged
+     */
+    public void addAll(Collection other) {
+        class AddProcessor extends Processor {
+            public void process(Object value) {
+                add(value);
+            }
+        }
+
+        other.forEach(new AddProcessor());
+    }
+
+    /**
+     * Removes all elements from the collection.
+     */
+    public void clear() {}
+}
