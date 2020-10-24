bc94972515b937029e9fd6589f01dbe94bb37d53
Borna Cafuk <borna.cafuk@gmail.com>
2020-10-24 11:01:17 +0200

    Add LexerError class

---
+++ hw02/src/main/java/hr/fer/oprpp1/hw02/prob1/LexerError.java
@@ -0,0 +1,37 @@
+package hr.fer.oprpp1.hw02.prob1;
+
+/**
+ * Represents an error that occurred when a {@link Lexer} was performing one of its operations.
+ *
+ * @author Borna Cafuk
+ */
+public class LexerError extends RuntimeException {
+    /**
+     * Constructs a new lexer error with the specified detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public LexerError(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new runtime exception with the specified detail message and
+     * cause. <p>Note that the detail message associated with
+     * {@code cause} is <i>not</i> automatically incorporated in
+     * this runtime exception's detail message.
+     *
+     * @param message the detail message (which is saved for later retrieval
+     *                by the {@link #getMessage()} method).
+     * @param cause   the cause (which is saved for later retrieval by the
+     *                {@link #getCause()} method).  (A {@code null} value is
+     *                permitted, and indicates that the cause is nonexistent or
+     *                unknown.)
+     */
+    public LexerError(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
