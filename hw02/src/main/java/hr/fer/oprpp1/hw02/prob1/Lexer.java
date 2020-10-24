a571577ed8662cf7eda800da06c806368bde63d2
Borna Cafuk <borna.cafuk@gmail.com>
2020-10-24 11:06:12 +0200

    Add Lexer class

---
+++ hw02/src/main/java/hr/fer/oprpp1/hw02/prob1/Lexer.java
@@ -0,0 +1,54 @@
+package hr.fer.oprpp1.hw02.prob1;
+
+/**
+ * Splits an input string into tokens according to the rules specified in the assignment document.
+ *
+ * @author Borna Cafuk
+ */
+public class Lexer {
+    /**
+     * The input data.
+     */
+    private char[] data;
+    /**
+     * The current token, i.e. the last token returned by {@link #nextToken()}
+     */
+    private Token token = null;
+    /**
+     * The index of the first character which has not yet been handled.
+     */
+    private int currentIndex = 0;
+
+    /**
+     * Creates a lexer for a given string.
+     *
+     * @param text the text which will be tokenized by this lexer
+     */
+    public Lexer(String text) {
+        data = text.toCharArray();
+    }
+
+    /**
+     * Gets the next from the input token and returns it.
+     *
+     * @return the token processed from the input
+     * @throws LexerError if there is an error while getting the next token
+     */
+    public Token nextToken() {
+        // TODO: Implement method
+        return null;
+    }
+
+    /**
+     * Gets the last token returned by {@link #nextToken()}.
+     *
+     * @return the last token processed from the input
+     * @throws LexerError if {@link #nextToken()} has not yet been called
+     */
+    public Token getToken() {
+        if (token == null)
+            throw new LexerError("nextToken has not yet been called.");
+
+        return token;
+    }
+}
