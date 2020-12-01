f59468e85be19723117b46f2dc34bf3359364059
Borna Cafuk <borna.cafuk@gmail.com>
2020-12-01 21:02:13 +0100

    Implement the methods in Util

--- hw05/src/main/java/hr/fer/oprpp1/hw05/crypto/Util.java
+++ hw05/src/main/java/hr/fer/oprpp1/hw05/crypto/Util.java
@@ -1,5 +1,7 @@
 package hr.fer.oprpp1.hw05.crypto;
 
+import java.util.Objects;
+
 /**
  * A collection of static utility methods used to convert between hexadecimal strings and arrays of bytes.
  *
@@ -32,7 +34,19 @@ public final class Util {
      * @throws IllegalArgumentException if {@code keyText} contains any characters other than hexadecimal digits
      */
     public static byte[] hextobyte(String keyText) {
-        throw new UnsupportedOperationException("Not yet implemented.");
+        Objects.requireNonNull(keyText, "The hexadecimal string must not be null.");
+
+        if (keyText.length() % 2 == 1)
+            throw new IllegalArgumentException("The length of the key text must be even, but was " + keyText.length() + ".");
+
+        byte[] byteArray = new byte[keyText.length() / 2];
+
+        for (int i = 0; i < keyText.length(); i += 2) {
+            int value = digitToValue(keyText.charAt(i)) * 16 + digitToValue(keyText.charAt(i + 1));
+            byteArray[i / 2] = (byte) ((value < 128) ? value : value - 256);
+        }
+
+        return byteArray;
     }
 
     /**
@@ -52,6 +66,53 @@ public final class Util {
      * @throws NullPointerException if {@code bytearray} is {@code null}
      */
     public static String bytetohex(byte[] bytearray) {
-        throw new UnsupportedOperationException("Not yet implemented.");
+        Objects.requireNonNull(bytearray, "The byte array must not be null.");
+
+        StringBuilder sb = new StringBuilder();
+
+        for (byte b : bytearray) {
+            int value = (b >= 0) ? b : b + 256;
+            sb.append(valueToDigit(value / 16))
+              .append(valueToDigit(value % 16));
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Converts an integer into a lowercase hexadecimal digit character.
+     *
+     * @param value the value to convert
+     * @return the resulting hexadecimal digit
+     * @throws IllegalArgumentException if {@code value} is less than 0 or greater than 15
+     */
+    private static char valueToDigit(int value) {
+        if (value >= 0 && value <= 9)
+            return (char) (value + '0');
+
+        if (value >= 10 && value <= 15)
+            return (char) (value + 'a' - 10);
+
+        throw new IllegalArgumentException("Only numbers 0 through 15 can be converted, but " + value + " was passed.");
+    }
+
+    /**
+     * Converts a hexadecimal digit into its numeric value.
+     *
+     * @param digit the character to convert
+     * @return the character's value in hexadecimal
+     * @throws IllegalArgumentException if {@code digit} is not a valid hexadecimal digit
+     */
+    private static int digitToValue(char digit) {
+        if (digit >= '0' && digit <= '9')
+            return digit - '0';
+
+        if (digit >= 'A' && digit <= 'F')
+            return digit - 'A' + 10;
+
+        if (digit >= 'a' && digit <= 'f')
+            return digit - 'a' + 10;
+
+        throw new IllegalArgumentException("Invalid hexadecimal character '" + digit + "'.");
     }
 }
