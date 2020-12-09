3073cba2100a29f14133f45baa5b9467c4fdcdf6
Borna Cafuk <borna.cafuk@gmail.com>
2020-12-09 20:13:44 +0100

    Implement ComplexRootedPolynomial methods

--- hw06/src/main/java/hr/fer/zemris/math/ComplexRootedPolynomial.java
+++ hw06/src/main/java/hr/fer/zemris/math/ComplexRootedPolynomial.java
@@ -1,5 +1,7 @@
 package hr.fer.zemris.math;
 
+import java.util.Objects;
+
 /**
  * Represents a complex polynomial in its factored form, i.e.
  * <i>f</i>(<i>z</i>) = <i>z</i><sub>0</sub> &middot; (<i>z</i>&minus;<i>z</i><sub>1</sub>) &middot;
@@ -9,6 +11,15 @@ package hr.fer.zemris.math;
  */
 public class ComplexRootedPolynomial {
     /**
+     * The constant multiplier <i>z</i><sub>0</sub>.
+     */
+    private final Complex constant;
+    /**
+     * The roots of the polynomial: <i>z</i><sub>1</sub>, <i>z</i><sub>2</sub>, &hellip; <i>z</i><sub><i>n</i></sub>
+     */
+    private final Complex[] roots;
+
+    /**
      * Creates a new polynomial.
      *
      * @param constant the constant factor <i>z</i><sub>0</sub>
@@ -17,7 +28,13 @@ public class ComplexRootedPolynomial {
      * @throws NullPointerException if {@code constant}, {@code roots}, or any element of {@code roots} is {@code null}
      */
     public ComplexRootedPolynomial(Complex constant, Complex... roots) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        Objects.requireNonNull(constant, "The constant must not be null");
+        Objects.requireNonNull(roots, "The roots must not be null");
+        for (Complex root : roots)
+            Objects.requireNonNull(root, "All of the roots must not be null");
+
+        this.constant = constant;
+        this.roots = roots;
     }
 
     /**
@@ -28,7 +45,13 @@ public class ComplexRootedPolynomial {
      * @throws NullPointerException if {@code z} is {@code null}
      */
     public Complex apply(Complex z) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        Objects.requireNonNull(z, "z must not be null");
+
+        Complex value = constant;
+        for (Complex root : roots)
+            value = value.multiply(z.sub(root));
+
+        return value;
     }
 
     /**
@@ -37,12 +60,28 @@ public class ComplexRootedPolynomial {
      * @return the corresponding {@link ComplexPolynomial}
      */
     public ComplexPolynomial toComplexPolynom() {
-        throw new UnsupportedOperationException("Not yet implemented");
+        ComplexPolynomial result = new ComplexPolynomial(constant);
+
+        for (Complex root : roots)
+            result = result.multiply(new ComplexPolynomial(root, Complex.ONE));
+
+        return result;
     }
 
     @Override
     public String toString() {
-        throw new UnsupportedOperationException("Not yet implemented");
+        StringBuilder sb = new StringBuilder();
+
+        sb.append('(')
+          .append(constant)
+          .append(')');
+
+        for (Complex root : roots)
+            sb.append(" * (z-(")
+              .append(root)
+              .append("))");
+
+        return sb.toString();
     }
 
     /**
@@ -56,8 +95,29 @@ public class ComplexRootedPolynomial {
      * @return the index of the root closest to {@code z} (0 for <i>z</i><sub>0</sub>, 1 for
      * <i>z</i><sub>2</sub>, &hellip; <i>k</i> &minus; 1 for <i>z</i><sub>k</sub>, &hellip;), or &minus;1 if none of
      * the roots' distance from  {@code z} is less than {@code treshold}
+     * @throws NullPointerException     if {@code z} is {@code null}
+     * @throws IllegalArgumentException if {@code treshold} is NaN
      */
     public int indexOfClosestRootFor(Complex z, double treshold) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        Objects.requireNonNull(z, "z must not be null");
+        if (Double.isNaN(treshold))
+            throw new IllegalArgumentException("The threshold must not be NaN");
+
+        double minDist = Double.POSITIVE_INFINITY;
+        int closestIndex = -1;
+
+        for (int i = 0; i < roots.length; i++) {
+            double dist = z.sub(roots[i]).module();
+
+            if (dist < minDist) {
+                minDist = dist;
+                closestIndex = i;
+            }
+        }
+
+        if (minDist <= treshold)
+            return closestIndex;
+        else
+            return -1;
     }
 }
